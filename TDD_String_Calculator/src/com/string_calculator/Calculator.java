6d651bdc17c92aa69448505e326cb82f92cb0a3e
Kuntala Sett <kuntala.sett.6174@gmail.com>
2021-02-21 22:47:25 +0530

    Commit for 0 or 1 case

--- TDD_String_Calculator/src/com/string_calculator/Calculator.java
+++ TDD_String_Calculator/src/com/string_calculator/Calculator.java
@@ -2,4 +2,10 @@ package com.string_calculator;
 
 public class Calculator {
 
+	public static int add(String nums){
+		int sum = 0;
+		if(nums.isBlank())
+			sum = 0;
+		return sum;
+	}
 }
--- TDD_String_Calculator/src/com/string_calculator/CalculatorTestClass.java
+++ TDD_String_Calculator/src/com/string_calculator/CalculatorTestClass.java
@@ -7,8 +7,13 @@ import org.junit.jupiter.api.Test;
 class CalculatorTestClass {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void returnZeroForEmptyString(){
+		assertEquals(0, Calculator.add(""));
 	}
-
+	
+	@Test
+	void returnNumberForSingleNumber(){
+		assertEquals(4, Calculator.add("4"));
+	}
+	
 }
